fb5e10cda4a2ae34c267251b14d33729987f0aef
luca giulianini <luca.giulianini2@studio.unibo.it>
2020-09-17 23:31:29 +0200

    Refactored Mediator

--- src/main/scala/it/unibo/pps1920/motoscala/controller/mediation/Mediator.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/controller/mediation/Mediator.scala
@@ -1,30 +1,29 @@
 package it.unibo.pps1920.motoscala.controller.mediation
 
-trait MediatorLike {
+import scala.reflect.ClassTag
 
-}
+trait MediatorLike
 
 trait EventObserver[T] {
   def notify(event: T): Unit
 }
 
 trait EventSubject {
-  import scala.reflect.ClassTag
-  def subscribe[T](observer: EventObserver[T])(implicit eventType: ClassTag[T]): Unit
-  def unsubscribe[T](observer: EventObserver[T])
-  def publishEvent[T](ev: T)(implicit eventType: ClassTag[T]): Unit
+  def subscribe[T: ClassTag](observer: EventObserver[T]): Unit
+  def unsubscribe[T](observer: EventObserver[T]): Unit
+  def publishEvent[T: ClassTag](ev: T): Unit
 }
 
 class Mediator private() extends EventSubject {
-  import scala.reflect.ClassTag
-  private var observers: Map[EventObserver[_], Class[_]] = Map()
-  override def publishEvent[T](ev: T)(implicit eventType: ClassTag[T]): Unit = {
-    observers.filter(o => o._2.isAssignableFrom(eventType.runtimeClass))
+  type EventType = Class[_]
+  private var observers: Map[EventObserver[_], EventType] = Map()
+
+  override def publishEvent[T: ClassTag](ev: T): Unit = {
+    observers.filter(o => o._2.isAssignableFrom(implicitly[ClassTag[T]].runtimeClass))
       .foreach(t => t._1.asInstanceOf[EventObserver[T]].notify(ev))
   }
-  override def subscribe[T](observer: EventObserver[T])(implicit eventType: ClassTag[T]): Unit = {
-    observers = observers + (observer -> eventType.runtimeClass)
-  }
+  override def subscribe[T: ClassTag](observer: EventObserver[T]): Unit =
+    observers = observers + (observer -> implicitly[ClassTag[T]].runtimeClass)
   override def unsubscribe[T](observer: EventObserver[T]): Unit = observers = observers - observer
 }
 
@@ -32,7 +31,7 @@ class Observer1 extends EventObserver[Event] {
   override def notify(event: Event): Unit = event match {
     case UpdateEvent(k) => println(k)
     case DrawEvent(k) => println(k)
-    case _ => println("not my message")
+    case CommandEvent(k) => println("Command event extends from Event")
   }
 }
 class Observer2 extends EventObserver[EventProva] {
@@ -40,18 +39,20 @@ class Observer2 extends EventObserver[EventProva] {
     case CommandEvent(k) => println("Observer2:" + k)
   }
 }
+
+object Mediator {
+  def apply(): Mediator = new Mediator()
+}
+
 object prova extends App {
   val mediator = Mediator()
   val observer = new Observer1()
   val observer2 = new Observer2()
-  mediator.subscribe[Event](observer)
-  mediator.subscribe[EventProva](observer2)
-  mediator.unsubscribe(observer)
+  mediator.subscribe(observer)
+  mediator.subscribe(observer2)
+  //mediator.unsubscribe(observer)
   mediator.publishEvent[DrawEvent](new DrawEvent(234))
   mediator.publishEvent[UpdateEvent](new UpdateEvent(234))
   mediator.publishEvent[EventProva](new CommandEvent(302))
 }
 
-object Mediator {
-  def apply(): Mediator = new Mediator()
-}
