9d4a66c8081a9f79345a288bf6d69c2fcbe35bb5
Blaac <ruben.ceroni@studio.unibo.it>
2020-09-25 15:22:43 +0200

    add test for vector and doc

--- src/main/scala/it/unibo/pps1920/motoscala/ecs/components/IntangibleComponent.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/ecs/components/IntangibleComponent.scala
@@ -2,4 +2,7 @@ package it.unibo.pps1920.motoscala.ecs.components
 
 import it.unibo.pps1920.motoscala.ecs.Component
 
+/**
+ * Component for intangible entities e.g. floor tiles
+ */
 case object IntangibleComponent extends Component
