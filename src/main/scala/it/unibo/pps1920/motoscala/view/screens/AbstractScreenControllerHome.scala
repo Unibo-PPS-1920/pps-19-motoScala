ec9d9a241829de0afd57651c6d3488be8b4c8758
Gyordan <gyordan.caminati@studio.unibo.it>
2020-09-27 20:35:26 +0200

    Pulizia struttura generale

--- src/main/scala/it/unibo/pps1920/motoscala/view/screens/AbstractScreenControllerHome.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/view/screens/AbstractScreenControllerHome.scala
@@ -4,76 +4,93 @@ import it.unibo.pps1920.motoscala.controller.UpdatableUI
 import it.unibo.pps1920.motoscala.view.ViewFacade
 import javafx.fxml.FXML
 import javafx.scene.control.Button
-import javafx.scene.effect.{BoxBlur, DropShadow}
+import javafx.scene.effect.DropShadow
 import javafx.scene.image.ImageView
 import javafx.scene.layout.BorderPane
 import javafx.scene.paint.Color
 import javafx.scene.shape.Line
+import javafx.stage
+import scalafx.animation.{Timeline, TranslateTransition}
+import scalafx.scene.shape.Rectangle
+import scalafx.util.Duration
 
 abstract class AbstractScreenControllerHome(protected override val viewFacade: ViewFacade,
                                             protected override val controller: UpdatableUI) extends ScreenController(viewFacade, controller) {
-  import javafx.scene.layout.GridPane
+  import javafx.scene.layout.{AnchorPane, GridPane}
+
+  private final val lineDuration = 5000
+  private final val pixelLineBound = 10000
+  private final val screenLineDivider = 10
+  private final val shadowBox = 50
+  private final val animationRate = 0.5
+  private final val lineStroke = 3
+  private final val xLineDivederBound = (-5, 10)
+  private final val yLineDivederBound = (1, 10)
+  private final val lineColor = Color.WHITE
+  private final val backgroundColor = Color.rgb(12, 45, 74)
 
 
-  private val shadow = new DropShadow(10, Color.BLACK)
-  private val blur = new BoxBlur(1, 1, 3)
-  @FXML protected var title: ImageView = _
+  private val shadow = new DropShadow(shadowBox, Color.BLACK)
   @FXML protected var root: BorderPane = _
+  @FXML protected var mainAnchorPane: AnchorPane = _
+  @FXML protected var mainGridPane: GridPane = _
+  @FXML protected var title: ImageView = _
   @FXML protected var textPlay: Button = _
+  @FXML protected var textPlayMultiplayer: Button = _
   @FXML protected var textSettings: Button = _
   @FXML protected var textStats: Button = _
   @FXML protected var textExit: Button = _
-  @FXML protected var mainGridPane: GridPane = _
-
   @FXML override def initialize(): Unit = {
-    import javafx.stage
-    import scalafx.animation.{Timeline, TranslateTransition}
-    import scalafx.util.Duration
     assertNodeInjected()
+
     val screenHeight = stage.Screen.getScreens.get(0).getBounds.getHeight
     val screenWidth = stage.Screen.getScreens.get(0).getBounds.getWidth
-    val yPortions = screenHeight / 10
-    val xPortions = screenWidth / 8
+    val yPortions = screenHeight / screenLineDivider
+    val xPortions = screenWidth / screenLineDivider
 
-    -5 to 10 foreach (i => {
+    this.mainGridPane.setEffect(shadow)
 
-      val yLine = new Line(-10000, yPortions * i, 10000, yPortions * i)
-      yLine.setStrokeWidth(3)
+    //Vertical line setup
+    xLineDivederBound._1 to xLineDivederBound._2 foreach (divider => {
       val translate: TranslateTransition = new TranslateTransition
-      translate.setDuration(Duration.apply(5000))
-      translate.setToY(screenHeight + yPortions * i)
+      val yLine = new Line(-pixelLineBound, yPortions * divider, pixelLineBound, yPortions * divider)
+      yLine.setStroke(lineColor)
+      yLine.setStrokeWidth(lineStroke)
+      translate.setDuration(Duration.apply(lineDuration))
+      translate.setToY(screenHeight + yPortions * divider)
       translate.setByY(0)
-      translate.setRate(0.5)
+      translate.setRate(animationRate)
       translate.setCycleCount(Timeline.Indefinite)
       translate.setNode(yLine)
       translate.setAutoReverse(true)
       translate.play()
       this.root.getChildren.add(0, yLine)
-
     })
 
-    1 to 8 foreach (i => {
-      val xLine = new Line(xPortions * i, -10000, xPortions * i, 10000)
-      xLine.setStrokeWidth(3)
+    //Horizontal line setup
+    yLineDivederBound._1 to yLineDivederBound._2 foreach (divider => {
+      val xLine = new Line(xPortions * divider, -pixelLineBound, xPortions * divider, pixelLineBound)
+      xLine.setStroke(lineColor)
+      xLine.setStrokeWidth(lineStroke)
       this.root.getChildren.add(0, xLine)
     })
 
-
-    println("sss" +
-              this.root.getWidth)
-    println("ssss" +
-              this.root.getHeight)
-
-
-    println(stage.Screen.getScreens.get(0).getBounds.getHeight)
-    println(stage.Screen.getScreens.get(0).getBounds.getWidth)
-
-
-    mainGridPane.setEffect(shadow)
+    //Background setup
+    val background: Rectangle = new Rectangle();
+    background.width = screenWidth
+    background.height = screenHeight
+    background.setFill(backgroundColor);
+    this.root.getChildren.add(0, background)
 
   }
   private def assertNodeInjected(): Unit = {
     assert(root != null, "fx:id=\"root\" was not injected: check your FXML file 'Home.fxml'.")
+    assert(mainAnchorPane != null, "fx:id=\"mainAnchorPane\" was not injected: check your FXML file 'Home.fxml'.")
+    assert(mainGridPane != null, "fx:id=\"mainGridPane\" was not injected: check your FXML file 'Home.fxml'.")
+    assert(textPlay != null, "fx:id=\"textPlay\" was not injected: check your FXML file 'Home.fxml'.")
+    assert(textSettings != null, "fx:id=\"textSettings\" was not injected: check your FXML file 'Home.fxml'.")
+    assert(textStats != null, "fx:id=\"textStats\" was not injected: check your FXML file 'Home.fxml'.")
+    assert(textExit != null, "fx:id=\"textExit\" was not injected: check your FXML file 'Home.fxml'.")
   }
 
 
