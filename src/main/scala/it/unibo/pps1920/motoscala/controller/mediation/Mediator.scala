67c704b501cfe90a8b6fab775b642f9bee2af519
luca giulianini <luca.giulianini2@studio.unibo.it>
2020-09-27 17:31:47 +0200

    Refactored mediator

--- src/main/scala/it/unibo/pps1920/motoscala/controller/mediation/Mediator.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/controller/mediation/Mediator.scala
@@ -8,20 +8,18 @@ object Mediator {
   private class MediatorImpl extends Mediator {
     private var observers: Map[Observer, EventType] = Map()
 
-    override def publishEvent[T: ClassTag](ev: T): Unit = {
-      observers.filter(o => o._2.isAssignableFrom(implicitly[ClassTag[T]].runtimeClass))
-        .foreach(t => t._1.asInstanceOf[EventObserver[T]].notify(ev))
-    }
+    override def publishEvent[T: ClassTag](ev: T): Unit = observers
+      .filter(o => o._2.isAssignableFrom(implicitly[ClassTag[T]].runtimeClass))
+      .map(_._1.asInstanceOf[EventObserver[T]])
+      .foreach(_.notify(ev))
     override def subscribe[T: ClassTag](observer: EventObserver[T]*): Unit =
       observers = observers ++ observer.map(o => o -> implicitly[ClassTag[T]].runtimeClass).toSet
     override def unsubscribe[T](observer: EventObserver[T]*): Unit = observers = observers -- observer
   }
-
   object Types {
     type EventType = Class[_]
     type Observer = EventObserver[_]
   }
-
   def apply(): Mediator = new MediatorImpl()
 }
 
