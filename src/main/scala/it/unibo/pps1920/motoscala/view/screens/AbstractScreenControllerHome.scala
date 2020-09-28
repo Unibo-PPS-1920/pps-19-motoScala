b11ea64c0e82f941e83031006f1699f7ad615496
Gyordan <gyordan.caminati@studio.unibo.it>
2020-09-28 22:59:31 +0200

    moved some style constant in css, fix buttons initialize in init, and refactoring internal code in private method

--- src/main/scala/it/unibo/pps1920/motoscala/view/screens/AbstractScreenControllerHome.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/view/screens/AbstractScreenControllerHome.scala
@@ -3,34 +3,20 @@ package it.unibo.pps1920.motoscala.view.screens
 import it.unibo.pps1920.motoscala.controller.ObservableUI
 import it.unibo.pps1920.motoscala.view.ViewFacade
 import javafx.fxml.FXML
+import javafx.scene.CacheHint
 import javafx.scene.control.Button
-import javafx.scene.effect.DropShadow
 import javafx.scene.image.ImageView
 import javafx.scene.layout.BorderPane
-import javafx.scene.paint.Color
 import javafx.scene.shape.Line
-import javafx.stage
-import scalafx.animation.{Timeline, TranslateTransition}
+import scalafx.animation.TranslateTransition
 import scalafx.scene.shape.Rectangle
 import scalafx.util.Duration
 
 abstract class AbstractScreenControllerHome(protected override val viewFacade: ViewFacade,
                                             protected override val controller: ObservableUI) extends ScreenController(viewFacade, controller) {
-  import javafx.scene.layout.{AnchorPane, GridPane}
-
-  private final val lineDuration = 5000
-  private final val pixelLineBound = 10000
-  private final val screenLineDivider = 10
-  private final val shadowBox = 50
-  private final val animationRate = 0.5
-  private final val lineStroke = 3
-  private final val xLineDivederBound = (-5, 10)
-  private final val yLineDivederBound = (1, 10)
-  private final val lineColor = Color.WHITE
-  private final val backgroundColor = Color.rgb(12, 45, 74)
+  import javafx.scene.layout.{AnchorPane, GridPane, Pane}
 
 
-  private val shadow = new DropShadow(shadowBox, Color.BLACK)
   @FXML protected var root: BorderPane = _
   @FXML protected var mainAnchorPane: AnchorPane = _
   @FXML protected var mainGridPane: GridPane = _
@@ -42,46 +28,9 @@ abstract class AbstractScreenControllerHome(protected override val viewFacade: V
   @FXML protected var textExit: Button = _
   @FXML override def initialize(): Unit = {
     assertNodeInjected()
-
-    val screenHeight = stage.Screen.getScreens.get(0).getBounds.getHeight
-    val screenWidth = stage.Screen.getScreens.get(0).getBounds.getWidth
-    val yPortions = screenHeight / screenLineDivider
-    val xPortions = screenWidth / screenLineDivider
-
-    this.mainGridPane.setEffect(shadow)
-
-    //Vertical line setup
-    xLineDivederBound._1 to xLineDivederBound._2 foreach (divider => {
-      val translate: TranslateTransition = new TranslateTransition
-      val yLine = new Line(-pixelLineBound, yPortions * divider, pixelLineBound, yPortions * divider)
-      yLine.setStroke(lineColor)
-      yLine.setStrokeWidth(lineStroke)
-      translate.setDuration(Duration.apply(lineDuration))
-      translate.setToY(screenHeight + yPortions * divider)
-      translate.setByY(0)
-      translate.setRate(animationRate)
-      translate.setCycleCount(Timeline.Indefinite)
-      translate.setNode(yLine)
-      translate.setAutoReverse(true)
-      translate.play()
-      this.root.getChildren.add(0, yLine)
-    })
-
-    //Horizontal line setup
-    yLineDivederBound._1 to yLineDivederBound._2 foreach (divider => {
-      val xLine = new Line(xPortions * divider, -pixelLineBound, xPortions * divider, pixelLineBound)
-      xLine.setStroke(lineColor)
-      xLine.setStrokeWidth(lineStroke)
-      this.root.getChildren.add(0, xLine)
-    })
-
-    //Background setup
-    val background: Rectangle = new Rectangle()
-    background.width = screenWidth
-    background.height = screenHeight
-    background.setFill(backgroundColor)
-    this.root.getChildren.add(0, background)
-
+    this.initializeGrid(this.root)
+    this.initializeBackground(this.root)
+    this.initializeButtons()
   }
   private def assertNodeInjected(): Unit = {
     assert(root != null, "fx:id=\"root\" was not injected: check your FXML file 'Home.fxml'.")
@@ -92,6 +41,87 @@ abstract class AbstractScreenControllerHome(protected override val viewFacade: V
     assert(textSettings != null, "fx:id=\"textSettings\" was not injected: check your FXML file 'Home.fxml'.")
     assert(textStats != null, "fx:id=\"textStats\" was not injected: check your FXML file 'Home.fxml'.")
     assert(textExit != null, "fx:id=\"textExit\" was not injected: check your FXML file 'Home.fxml'.")
+  }
+  private def initializeButtons(): Unit = {
+    this.textPlay.setOnAction(_ => {
+      viewFacade.changeScreen(ScreenEvent.GotoGame)
+    })
+
+    this.textPlayMultiplayer.setOnAction(_ => {
+      viewFacade.changeScreen(ScreenEvent.GotoLobby)
+    })
+
+    this.textSettings.setOnAction(_ => {
+      viewFacade.changeScreen(ScreenEvent.GotoSettings)
+    })
+
+    this.textStats.setOnAction(_ => {
+      viewFacade.changeScreen(ScreenEvent.GotoStats)
+    })
+
+
+    this.textExit.setOnAction(_ => {
+      import javafx.application.Platform
+      Platform.exit()
+    })
+  }
+
+  private def initializeBackground(pane: Pane): Unit = {
+    import it.unibo.pps1920.motoscala.view.utilities.ViewUtils.GlobalViewConstants
+    //Background setup
+    val background: Rectangle = new Rectangle()
+    background.width = GlobalViewConstants.screenWidth
+    background.height = GlobalViewConstants.screenHeight
+    background.setCache(true)
+    background.setCacheHint(CacheHint.SPEED)
+    background.setId(Constant.cssBackgroundID)
+    pane.getChildren.add(0, background)
+  }
+  private def initializeGrid(pane: Pane): Unit = {
+    import it.unibo.pps1920.motoscala.view.utilities.ViewUtils.GlobalViewConstants
+
+    //Vertical line setup
+    Constant.xLineDividerBound._1 to Constant.xLineDividerBound._2 foreach (multiplayer => {
+      import scalafx.animation.Timeline
+      val xLine = new Line(0, Constant.xPortions * multiplayer, Constant.pixelLineBound, Constant
+        .xPortions * multiplayer)
+      xLine.setCache(true)
+      xLine.setCacheHint(CacheHint.SPEED)
+      xLine.setId(Constant.cssGridLineID)
+      val translate: TranslateTransition = new TranslateTransition
+      translate.setDuration(Duration.apply(Constant.animationLineDuration * 2))
+      translate.setToY((GlobalViewConstants.screenHeight + Constant.yPortions * multiplayer) / 2)
+      translate.setCycleCount(Timeline.Indefinite)
+      translate.setNode(xLine)
+      translate.setAutoReverse(true)
+      translate.play()
+      this.root.getChildren.add(0, xLine)
+    })
+
+
+    //Horizontal line setup
+    Constant.yLineDividerBound._1 to Constant.yLineDividerBound._2 foreach (multiplayer => {
+      val yLine = new Line(Constant.yPortions * multiplayer, -Constant.pixelLineBound, Constant
+        .yPortions * multiplayer, Constant.pixelLineBound)
+      yLine.setCache(true)
+      yLine.setCacheHint(CacheHint.SPEED)
+      yLine.setId(Constant.cssGridLineID)
+      pane.getChildren.add(0, yLine)
+    })
+  }
+
+  private[this] final object Constant {
+    import it.unibo.pps1920.motoscala.view.utilities.ViewUtils.GlobalViewConstants
+    final val animationLineDuration = 5000
+    final val pixelLineBound = 5000
+    final val screenLineDivider = 10
+    final val xLineDividerBound = (-2, 10)
+    final val yLineDividerBound = (1, 10)
+    final val xPortions = GlobalViewConstants.screenHeight / screenLineDivider
+    final val yPortions = GlobalViewConstants.screenWidth / screenLineDivider
+    final val cssGridLineID = "Line"
+    final val cssBackgroundID = "Background"
+
   }
 
 
