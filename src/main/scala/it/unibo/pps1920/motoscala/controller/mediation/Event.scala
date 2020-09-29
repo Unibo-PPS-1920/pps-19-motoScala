1bf4a1bb94ecb8891c92ff8c71a9fa57948556ce
luca giulianini <luca.giulianini2@studio.unibo.it>
2020-09-29 14:05:47 +0200

    Refactored event

--- src/main/scala/it/unibo/pps1920/motoscala/controller/mediation/Event.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/controller/mediation/Event.scala
@@ -1,36 +1,26 @@
 package it.unibo.pps1920.motoscala.controller.mediation
 
+import it.unibo.pps1920.motoscala.ecs.Entity
+import it.unibo.pps1920.motoscala.ecs.components.Shape.Shape
+import it.unibo.pps1920.motoscala.ecs.util.Direction.Direction
+import it.unibo.pps1920.motoscala.ecs.util.Vector2
+
 sealed trait Event
 
 object Event {
-
-
   type EntityData = EventData.EntityData
   type LevelSetupData = String
   type LevelEndData = String
   type CommandData = EventData.CommandData
-
   sealed trait DisplayableEvent extends Event
-
   final case class DrawEntityEvent(entity: Seq[EntityData]) extends DisplayableEvent
-
   final case class LevelSetupEvent(data: LevelSetupData) extends DisplayableEvent
-
   final case class LevelEndEvent(data: LevelEndData) extends DisplayableEvent
-
   sealed trait CommandableEvent extends Event
-
   final case class CommandEvent(cmd: Event.CommandData) extends CommandableEvent
-
 }
 
 object EventData {
-  import it.unibo.pps1920.motoscala.ecs.Entity
-  import it.unibo.pps1920.motoscala.ecs.components.Shape.Shape
-  import it.unibo.pps1920.motoscala.ecs.util.Direction.Direction
-  import it.unibo.pps1920.motoscala.ecs.util.Vector2
   final case class CommandData(entity: Entity, direction: Direction)
-
   final case class EntityData(vec: Vector2, shape: Shape, enType: EntityType)
-
 }
