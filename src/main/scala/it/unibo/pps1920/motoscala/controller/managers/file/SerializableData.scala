b3c5a36218f2506b3f28785c2cd7c7f03d2d0833
Gyordan <gyordan.caminati@studio.unibo.it>
2020-09-30 11:01:41 +0200

    Added base structure for serializable data

--- src/main/scala/it/unibo/pps1920/motoscala/controller/managers/file/SerializableData.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/controller/managers/file/SerializableData.scala
@@ -1,8 +1,15 @@
 package it.unibo.pps1920.motoscala.controller.managers.file
 
+import com.fasterxml.jackson.annotation.{JsonAutoDetect, JsonProperty}
+
 object SerializableData {
   case class dataSettings()
   case class dataScores()
-  case class dataLevel()
+
 
 }
+
+case class DataLevel(
+  @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+  @JsonProperty val xx: Int
+)
