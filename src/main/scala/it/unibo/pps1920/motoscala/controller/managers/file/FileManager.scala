58ced4a6d0d8bcded13ba695d2313a11218c72f7
Gyordan <gyordan.caminati@studio.unibo.it>
2020-09-30 11:22:58 +0200

    Added some feature to file manager for yaml test

--- src/main/scala/it/unibo/pps1920/motoscala/controller/managers/file/FileManager.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/controller/managers/file/FileManager.scala
@@ -3,7 +3,7 @@ package it.unibo.pps1920.motoscala.controller.managers.file
 import java.io.File
 import java.nio.file.{Path, Paths}
 
-import it.unibo.pps1920.motoscala.controller.managers.file.FileConstants.{APP_SCORE_FOLDER, APP_SETTINGS_FOLDER, SYSTEM_DIVIDER}
+import it.unibo.pps1920.motoscala.controller.managers.file.FileConstants.{APP_SCORE_FOLDER, APP_SETTINGS_FOLDER, SYSTEM_SEPARATOR}
 import org.slf4j.LoggerFactory
 
 import scala.util.Try
@@ -12,29 +12,29 @@ object FileManager {
 
   private final val logger = LoggerFactory getLogger getClass
   final implicit def stringPathToPath(path: String): Path = Paths.get(path)
-  final def createLocalDirectoryTree(path: Path)(): Boolean = Try(path.toFile.mkdirs())
+  final def createLocalDirectoryTree(path: Path)(): Boolean = tryAndReturn(path.toFile.getParentFile.mkdirs())
+  final def createLocalDirectory(path: Path): Boolean = tryAndReturn(path.toFile.mkdir)
+  final def createLocalFile(path: Path): Boolean = tryAndReturn(path.toFile.createNewFile())
+  final def deleteLocalFile(path: Path): Boolean = tryAndReturn(path.toFile.delete())
+  private final def tryAndReturn[T](operation: => T): Boolean = Try(operation)
     .fold(error => {logger.warn(error.getMessage); false }, _ => true)
-  final def createLocalDirectory(path: Path): Boolean = Try(path.toFile.mkdir)
-    .fold(error => {logger.warn(error.getMessage); false }, _ => true)
-  final def createLocalFile(path: String): Boolean = ???
-  final def deleteLocalFile(path: String): Boolean = ???
   final def loadLocalFile(): Unit = ???
   final def resolveRelativePath(): Unit = ???
   final def resolveAbsolutePath(): Unit = ???
 }
 
 object FileConstants {
-  final val SYSTEM_DIVIDER: String = File.separator
+  final val SYSTEM_SEPARATOR: String = File.separator
   final val USER_HOME: String = System.getProperty("user.home")
   final val APP_MAIN_FOLDER: String = USER_HOME + "MotoScala"
-  final val APP_SETTINGS_FOLDER: String = USER_HOME + SYSTEM_DIVIDER + "Settings"
-  final val APP_SCORE_FOLDER: String = USER_HOME + SYSTEM_DIVIDER + "Score"
+  final val APP_SETTINGS_FOLDER: String = USER_HOME + SYSTEM_SEPARATOR + "Settings"
+  final val APP_SCORE_FOLDER: String = USER_HOME + SYSTEM_SEPARATOR + "Score"
 }
 
 
 object ResourcesPaths {
-  final val STATS_FILE: String = APP_SCORE_FOLDER + SYSTEM_DIVIDER + "Stats.yml"
-  final val SETTINGS_FILE: String = APP_SETTINGS_FOLDER + SYSTEM_DIVIDER + "Stats.yml"
+  final val STATS_FILE: String = APP_SCORE_FOLDER + SYSTEM_SEPARATOR + "Stats.yml"
+  final val SETTINGS_FILE: String = APP_SETTINGS_FOLDER + SYSTEM_SEPARATOR + "Stats.yml"
 }
 
 
--- src/test/scala/it/unibo/pps1920/motoscala/controller/managers/file/YamlManagerTest.scala
+++ src/test/scala/it/unibo/pps1920/motoscala/controller/managers/file/YamlManagerTest.scala
@@ -2,6 +2,7 @@ package it.unibo.pps1920.motoscala.controller.managers.file
 
 import java.nio.file.Paths
 
+import it.unibo.pps1920.motoscala.controller.managers.file.FileConstants.{APP_MAIN_FOLDER, SYSTEM_SEPARATOR}
 import org.junit.runner.RunWith
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.should.Matchers
@@ -9,7 +10,7 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class YamlManagerTest extends AnyWordSpec with Matchers with BeforeAndAfterAll {
+abstract class YamlManagerTest extends AnyWordSpec with Matchers with BeforeAndAfterAll {
 
 
   var manager: YamlManager
@@ -22,13 +23,13 @@ class YamlManagerTest extends AnyWordSpec with Matchers with BeforeAndAfterAll {
   "A YamlManager" when {
     "Save o read file" should {
       "Save a yaml from file" in {
-        this.manager.saveYaml(Paths.get("/home/cam/Scaricati/test.yaml"))(TestClass(1)) shouldBe true
+        this.manager.saveYaml(Paths.get(APP_MAIN_FOLDER + SYSTEM_SEPARATOR + "Test.yaml"))(TestClass(1)) shouldBe true
       }
       "Read a yaml from file" in {
         this.manager
-          .loadYaml(Paths.get("/home/cam/Scaricati/test.yaml"))(classOf[DataLevel]) shouldBe Some(TestClass(1))
+          .loadYaml(Paths
+                      .get(APP_MAIN_FOLDER + SYSTEM_SEPARATOR + "Test.yaml"))(classOf[DataLevel]) shouldBe Some(TestClass(1))
       }
-
     }
   }
 
