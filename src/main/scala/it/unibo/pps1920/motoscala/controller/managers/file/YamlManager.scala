8eeebb40e6fca06e65e1b31dac4f1e161a4145ff
Gyordan <gyordan.caminati@studio.unibo.it>
2020-09-30 10:59:31 +0200

    Added base class for Yaml manager

--- src/main/scala/it/unibo/pps1920/motoscala/controller/managers/file/YamlManager.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/controller/managers/file/YamlManager.scala
@@ -4,15 +4,20 @@ import java.nio.file.Path
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import org.slf4j.LoggerFactory
+
+import scala.util.Try
 
 class YamlManager {
+  private final val logger = LoggerFactory getLogger classOf[YamlManager]
   private val mapper = new ObjectMapper(new YAMLFactory())
-  def loadYaml[T](location: Path): T = {
+  def loadYaml[T](location: Path)(cl: Class[T]): Option[T] = {
     mapper.findAndRegisterModules
-    val result = mapper.readValue(location.toFile, classOf[T])
-    result
+    val cls = manifest.runtimeClass.asInstanceOf[Class[T]]
+    Try(mapper.readValue(location.toFile, cl)).fold(err => {logger.warn(err.getMessage); None }, Some(_))
   }
-  def saveYaml[T](location: Path)(data: T): Unit = {
-    mapper.writeValue(location.toFile, data)
-  }
+
+  def saveYaml[T](location: Path)(data: T): Boolean = Try(mapper.writeValue(location.toFile, data))
+    .fold(err => {logger.warn(err.getMessage); false }, _ => true)
+
 }
