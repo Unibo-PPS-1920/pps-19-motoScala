8542811536aa1808e03813b4856bb697e8a7eefc
Blaac <ruben.ceroni@studio.unibo.it>
2020-10-01 13:04:21 +0200

    add angle getter to dir

--- src/main/scala/it/unibo/pps1920/motoscala/ecs/util/Direction.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/ecs/util/Direction.scala
@@ -10,6 +10,18 @@ sealed case class Direction(value: Vector2) {
   def +(dir: Direction): Direction = {
     Direction((dir.value add value).unit())
   }
+  def angle(dir: Direction): Int = dir match {
+    case Direction.Center => 0
+    case Direction.North => 0
+    case Direction.NorthWest => 315
+    case Direction.NorthEast => 45
+    case Direction.South => 180
+    case Direction.SouthWest => 225
+    case Direction.SouthEast => 135
+    case Direction.West => 270
+    case Direction.East => 90
+    case _ => -1
+  }
 }
 object Direction {
   object Center extends Direction(Vector2(0, 0))
