a06d7c86b07b126cb0754966d8290393bd92442b
Gyordan <gyordan.caminati@studio.unibo.it>
2020-10-02 22:10:01 +0200

    Added code for Data Manger and general refactor

--- src/main/scala/it/unibo/pps1920/motoscala/controller/managers/file/DataManager.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/controller/managers/file/DataManager.scala
@@ -1,32 +1,33 @@
 package it.unibo.pps1920.motoscala.controller.managers.file
 
 import it.unibo.pps1920.motoscala.controller.managers.file.FileConstants.{APP_MAIN_FOLDER, APP_SCORE_FOLDER, APP_SETTINGS_FOLDER, APP_USER_LEVEL_FOLDER}
-import it.unibo.pps1920.motoscala.controller.managers.file.FileManager.stringPathToPath
+import it.unibo.pps1920.motoscala.controller.managers.file.ResourcesPaths.{SCORE_FILE, SETTINGS_FILE}
 import it.unibo.pps1920.motoscala.model.Level.LevelData
+import it.unibo.pps1920.motoscala.model.Scores.Score
+import it.unibo.pps1920.motoscala.model.Settings.Setting
+import org.slf4j.LoggerFactory
 
-class DataManager {
+final class DataManager {
 
+  private final val logger = LoggerFactory getLogger classOf[DataManager]
   private val yamlManager: YamlManager = new YamlManager
+  def loadSettings(): Option[Setting] = yamlManager.loadYaml(SETTINGS_FILE)(classOf[Setting])
+  def saveSettings(sett: Setting): Boolean = yamlManager.saveYaml(SETTINGS_FILE)(sett)
+  def loadScore(): Option[Score] = yamlManager.loadYaml(SCORE_FILE)(classOf[Score])
+  def saveScore(scores: Score): Boolean = yamlManager.saveYaml(SCORE_FILE)(scores)
+  def initAppDirectory(): Boolean = {
+    tryAndBoolResult(FileManager.createLocalDirectoryTreeFromFile(APP_MAIN_FOLDER),
+                     FileManager.createLocalDirectory(APP_SETTINGS_FOLDER),
+                     FileManager.createLocalDirectory(APP_SCORE_FOLDER),
+                     FileManager.createLocalDirectory(APP_USER_LEVEL_FOLDER))((this.logger))
 
-
-  def loadSettings(): Unit = {}
-  def saveSettings(): Unit = ???
-  def loadScore(): Unit = ???
-  def saveScore(): Unit = ???
-  def initAppDirectory(): Unit = {
-    FileManager.createLocalDirectoryTreeFromFile(APP_MAIN_FOLDER)
-    FileManager.createLocalDirectory(APP_SETTINGS_FOLDER)
-    FileManager.createLocalDirectory(APP_SCORE_FOLDER)
-    FileManager.createLocalDirectory(APP_USER_LEVEL_FOLDER)
   }
   def loadUserLvl(): List[LevelData] = {
-    var lis: List[LevelData] = List()
-    FileManager.getListFiles(APP_USER_LEVEL_FOLDER).foreach(fileName => {
-      this.yamlManager.loadYaml(APP_USER_LEVEL_FOLDER + fileName)(classOf[LevelData]).foreach(lis.::(_))
-    })
-    lis
+    FileManager.getListFiles(APP_USER_LEVEL_FOLDER)
+      .map(pf => this.yamlManager.loadYaml(APP_USER_LEVEL_FOLDER + pf)(classOf[LevelData]))
+      .filter(_.isDefined).map(_.get)
   }
-  def loadLvl(): Unit = ???
-  def saveLvl(): Unit = ???
+
+  def loadLvl(): List[LevelData] = ???
 
 }
--- src/main/scala/it/unibo/pps1920/motoscala/model/Level.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/model/Level.scala
@@ -1,9 +1,11 @@
 package it.unibo.pps1920.motoscala.model
 
 import it.unibo.pps1920.motoscala.ecs.components.Shape.{Circle, Rectangle}
+import it.unibo.pps1920.motoscala.ecs.util.Vector2
 
 object Level {
   sealed trait LevelEntity
+  case class LevelData12(index: Int, mapSize: Vector2, entities: List[LevelEntity])
   case class LevelData(index: Int, mapSize: Coordinate, entities: List[LevelEntity])
   case class Coordinate(x: Int, y: Int)
   case class Tile(shape: Rectangle, position: Coordinate, tangible: Boolean) extends LevelEntity
@@ -11,10 +13,3 @@ object Level {
   case class Enemy1(position: Coordinate, shape: Rectangle, direction: Coordinate, velocity: Double) extends LevelEntity
 }
 
-/*package it.unibo.pps1920.motoscala.model
-
-object Level {
-  case class LevelData(index: Int, mapSize: (Int, Int), entities: List[LevelEntity])
-  case class LevelEntity(entityType: String, shape: ())
-}
-*/
