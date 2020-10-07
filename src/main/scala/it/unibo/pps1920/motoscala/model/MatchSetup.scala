501b965d6b761b16e8d70c20372d7fd6ad3d7645
Sara Kiade <sara.kiade@studio.unibo.it>
2020-10-07 10:04:55 +0200

    added match setup

--- src/main/scala/it/unibo/pps1920/motoscala/model/MatchSetup.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/model/MatchSetup.scala
@@ -1,5 +1,13 @@
 package it.unibo.pps1920.motoscala.model
-
+import akka.actor.ActorRef
 trait MatchSetup {
-
 }
+
+
+case class MultiPlayerSetup(difficulty: Int, mode: Boolean, numPlayers: Int) extends MatchSetup {
+  var readyPlayers : Map[ActorRef, Boolean] = Map()
+  def setReady(playerRef:ActorRef): Unit = readyPlayers+=(playerRef -> true)
+  def unsetReady(playerRef:ActorRef): Unit = readyPlayers+=(playerRef -> false)
+}
+
+case class SinglePlayerSetup(difficulty: Int) extends MatchSetup
