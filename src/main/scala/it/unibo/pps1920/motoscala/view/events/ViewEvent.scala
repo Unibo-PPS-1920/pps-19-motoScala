027831f63ae3df54f7142ab6647ce71ecb9685e1
Sara Kiade <sara.kiade@studio.unibo.it>
2020-10-10 12:57:48 +0200

    added lobby data

--- src/main/scala/it/unibo/pps1920/motoscala/view/events/ViewEvent.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/view/events/ViewEvent.scala
@@ -4,11 +4,13 @@ import it.unibo.pps1920.motoscala.model.Level.LevelData
 
 sealed trait ViewEvent
 object ViewEvent {
+  import it.unibo.pps1920.motoscala.multiplayer.messages.DataType.LobbyData
   sealed trait HomeEvent extends ViewEvent
   sealed trait LevelEvent extends ViewEvent
   final case class LevelDataEvent(levels: Seq[LevelData]) extends LevelEvent
   sealed trait GameEvent extends ViewEvent
   sealed trait LobbyEvent extends ViewEvent
+  final case class LobbyDataEvent(lobbyData : LobbyData) extends  LobbyEvent
   sealed trait SettingsEvent extends ViewEvent
   sealed trait StatsEvent extends ViewEvent
 }
