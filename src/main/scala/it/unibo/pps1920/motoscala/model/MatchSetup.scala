c359553e1cef50bb27cb7a3dbbe0cea368ecf4e9
Sara Kiade <sara.kiade@studio.unibo.it>
2020-10-10 12:55:09 +0200

    refactored match setup

--- src/main/scala/it/unibo/pps1920/motoscala/model/MatchSetup.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/model/MatchSetup.scala
@@ -6,8 +6,13 @@ trait MatchSetup {
 
 case class MultiPlayerSetup(difficulty: Int, mode: Boolean, numPlayers: Int) extends MatchSetup {
   var readyPlayers : Map[ActorRef, Boolean] = Map()
-  def setReady(playerRef:ActorRef): Unit = readyPlayers+=(playerRef -> true)
+  def setReady(playerRef:ActorRef): Unit = if(readyPlayers.contains(playerRef)) readyPlayers+=(playerRef -> true)
   def unsetReady(playerRef:ActorRef): Unit = readyPlayers+=(playerRef -> false)
+  def numReadyPlayers() : Int = readyPlayers.count(rp=> rp._2)
+  def tryAddPlayer(playerRef:ActorRef) : Boolean = {
+    if(readyPlayers.size < numPlayers) {readyPlayers+=(playerRef->false); true}
+    else false
+  }
 }
 
 case class SinglePlayerSetup(difficulty: Int) extends MatchSetup
