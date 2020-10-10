209d53c1ae4955bb18c3529b0c021e30a567b7a3
Sara Kiade <sara.kiade@studio.unibo.it>
2020-10-10 13:02:56 +0200

    Server: implemented missing receive msgs and refactored behaviours

--- src/main/scala/it/unibo/pps1920/motoscala/multiplayer/actors/ServerActor.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/multiplayer/actors/ServerActor.scala
@@ -1,27 +1,41 @@
 package it.unibo.pps1920.motoscala.multiplayer.actors
-import it.unibo.pps1920.motoscala.multiplayer.messages._
 import akka.actor.{Actor, ActorLogging}
 import it.unibo.pps1920.motoscala.controller.ActorController
-import it.unibo.pps1920.motoscala.multiplayer.messages.PlainMessage
+import it.unibo.pps1920.motoscala.multiplayer.messages.Message._
+import akka.actor.ActorRef
+import it.unibo.pps1920.motoscala.controller.mediation.Event.DisplayableEvent
+import it.unibo.pps1920.motoscala.controller.mediation.Mediator
+import akka.actor.Props
 class ServerActor (protected val actorController: ActorController) extends Actor with ActorLogging{
-  import akka.actor.ActorRef
-  import it.unibo.pps1920.motoscala.controller.mediation.Event.DisplayableEvent
-  import it.unibo.pps1920.motoscala.controller.mediation.Mediator
-  val controller : ActorController = actorController
-  val levelMediator : Mediator = controller.getMediator
-  var clients : Set[ActorRef] = Set()
-  def handle(displayEvent : DisplayableEvent): Unit = clients.foreach(c=> c.tell(DisplayMessage(displayEvent), this.self))
-  override def receive: Receive = {
 
+  import org.slf4j.LoggerFactory
+  private val controller : ActorController = actorController
+  private val levelMediator : Mediator = controller.getMediator
+  private var clients : Set[ActorRef] = Set()
+  private val logger = LoggerFactory getLogger classOf[ServerActor]
+  def handle(displayEvent : DisplayableEvent): Unit = clients.foreach(c=> c.tell(DisplayMessage(displayEvent), this.self))
+
+  override def receive: Receive = idleBehaviour
+
+  def inGameBehaviour : Receive= {
+    case CommandMessage(event) => controller.getMediator.publishEvent(event)
+    case msg => logger warn s"Received unexpected message ${msg}"; println(akka.serialization.Serialization.serializedActorPath(self))
+  }
+  def idleBehaviour : Receive = {
     case PlainMessage(text, num) => println("TESTO: "+text + "NUM:" + num)
-    case CommandMessage(event) => controller.notifyUsersUpdate(event)
-    case JoinRequestMessage => controller.notifyJoinRequest(this.sender())
-    case ReadyMessage => controller.notifyReady(this.sender())
-    case _ => println("AAAAAAA "+akka.serialization.Serialization.serializedActorPath(self))
+    case JoinRequestMessage => handleJoinRequest()
+    case ReadyMessage => controller.setReadyClient(this.sender())
+    case msg => logger warn s"Received unexpected message ${msg}"
+  }
+  private def handleJoinRequest(): Unit ={
+    val resp = controller.requestJoin(this.sender())
+    if(resp) {
+      this.sender().tell(JoinResponseMessage(resp), this.self)
+      this.sender().tell(LobbyDataMessage(controller.getLobbyData), this.self)
+    }
   }
 }
 
 object ServerActor {
-  import akka.actor.Props
   def props(controller: ActorController): Props = Props(new ServerActor(controller))
 }
