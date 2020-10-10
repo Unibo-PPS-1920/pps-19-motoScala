fb2aed39d65716dfb1ee436a0418313d1fe0e306
Sara Kiade <sara.kiade@studio.unibo.it>
2020-10-10 12:52:15 +0200

    redefined actor controller methods

--- src/main/scala/it/unibo/pps1920/motoscala/controller/ActorController.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/controller/ActorController.scala
@@ -4,16 +4,19 @@ trait ActorController {
   import akka.actor.ActorRef
   import it.unibo.pps1920.motoscala.controller.mediation.{Event, Mediator}
   import it.unibo.pps1920.motoscala.engine.Engine
-  import it.unibo.pps1920.motoscala.model.MatchSetup
-
-  def notifySettingsUpdate(setup : MatchSetup) : Unit
-  def notifyJoinRequest(ref : ActorRef) : Boolean
-  def notifyUsersUpdate(event : Event) : Unit
-  def notifyNewWorld(event: Event) : Unit
-  def notifyGameStart() : Unit
-  def notifyGameEnd() : Unit
-  def notifyReady(ref: ActorRef)  : Unit
+  import it.unibo.pps1920.motoscala.multiplayer.messages.MessageData.LobbyData
   def getMediator : Mediator
+  def getLobbyData : LobbyData
   def serverMode(gameEngine : Engine) : Unit
   def clientMode() : Unit
+  //Called by Server Actor
+  def requestJoin(ref : ActorRef) : Boolean
+  def setReadyClient(ref: ActorRef)  : Unit
+  //Called by Client Actor
+  def gameStart() : Unit
+  def gameEnd() : Unit
+  def settingsUpdate(lobbyData : LobbyData) : Unit
+
+
+
 }
