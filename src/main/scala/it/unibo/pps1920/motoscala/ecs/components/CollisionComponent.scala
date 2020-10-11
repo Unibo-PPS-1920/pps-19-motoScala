9ba0fb0f940bc39395abe20fae5b94625a49fea2
Sara Kiade <sara.kiade@studio.unibo.it>
2020-10-11 13:50:59 +0200

    refactored collision component

--- src/main/scala/it/unibo/pps1920/motoscala/ecs/components/CollisionComponent.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/ecs/components/CollisionComponent.scala
@@ -1,5 +1,16 @@
 package it.unibo.pps1920.motoscala.ecs.components
 
-class CollisionComponent {
+import it.unibo.pps1920.motoscala.ecs.Component
+import it.unibo.pps1920.motoscala.ecs.util.Vector2
 
-}
+/**
+ *
+ * @param mass determines how far the entity will bounce away
+ * @param duration keeps track of how long an active collision will last
+ * @param oldSpeed original speed to be restored after collision has happened
+ */
+final case class CollisionComponent(var mass: Double,
+                                    var isColliding: Boolean = false,
+                                    var duration: Int = 0,
+                                    var oldSpeed: Vector2) extends Component
+
