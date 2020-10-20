f6e79615619a7442271bd8fa6f4ecdb46868c82a
luca giulianini <luca.giulianini2@studio.unibo.it>
2020-10-21 01:30:28 +0200

    Bellissimo

--- src/main/scala/it/unibo/pps1920/motoscala/controller/managers/audio/Clips.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/controller/managers/audio/Clips.scala
@@ -3,8 +3,6 @@ package it.unibo.pps1920.motoscala.controller.managers.audio
 import enumeratum._
 
 sealed abstract class Clips(override val entryName: String) extends EnumEntry
-
-
 object Clips extends Enum[Clips] {
   override def values: IndexedSeq[Clips] = findValues
   case object Hit extends Clips("/clips/Hit.wav")
