5b5a39c6decd4ee4b06dd452e395f9b6a2f0362d
Sara Kiade <sara.kiade@studio.unibo.it>
2020-10-22 10:34:05 +0200

    refining documentation

--- src/main/scala/it/unibo/pps1920/motoscala/ecs/systems/Collision/package.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/ecs/systems/Collision/package.scala
@@ -5,16 +5,42 @@ import it.unibo.pps1920.motoscala.ecs.util.Side.Center
 import it.unibo.pps1920.motoscala.ecs.util.Alignment
 import scala.math.signum
 package object Collision {
-
+  /**
+   * Calculates mono-dimensional elastic collision between two elements.
+   *
+   * @param vel1 first element velocity
+   * @param vel2 second element velocity
+   * @param mass1 first element mass
+   * @param mass2 second element mass
+   * @return computed new scalar velocity
+   */
   def elasticCollision1D(vel1: Double, vel2: Double, mass1: Double, mass2: Double): Double = {
     var newVel = (vel1 * (mass1 - mass2) + 2 * mass2 * vel2) / (mass1 + mass2)
     val newVelSign = signum(newVel)
     if (newVel.toInt == 0) newVel = 1.0 * newVelSign
     newVel
   }
+  /**
+   * Detects collision between two circles
+   *
+   * @param cPos1 first circle position
+   * @param cPos2 second circle position
+   * @param cRadius1 first circle radius
+   * @param cRadius2 second circle radius
+   * @return Boolean true if circles are intersected
+   *         false otherwise
+   */
   def areCirclesTouching(cPos1: Vector2, cPos2: Vector2, cRadius1: Float, cRadius2: Float): Boolean =
     (cPos1 dist cPos2) <= (cRadius1 + cRadius2)
-  def getSide(refTopLeft: Vector2, refBottomRight: Vector2, point: Vector2): Alignment ={
+  /**
+   * Determines which side of a rectangle a point is on
+   *
+   * @param refTopLeft coordinates of the rectangle top left corner
+   * @param refBottomRight coordinates of the rectangle bottom right corner
+   * @param point the point to compare
+   * @return [[Alignment]]
+   */
+  def getRecSide(refTopLeft: Vector2, refBottomRight: Vector2, point: Vector2): Alignment ={
     val alignment = Alignment(Center, Center)
     if (point.x < refTopLeft.x) alignment.horizontalSide = Side.Left
     else if (point.x > refBottomRight.x) alignment.horizontalSide = Side.Right
@@ -31,7 +57,7 @@ package object Collision {
     val testEdge = Vector2(circlePos.x, circlePos.y)
     val invVec = Vector2(1, 1)
 
-    val alignment = Collision.getSide(topLeft, bottomRight, circlePos)
+    val alignment = Collision.getRecSide(topLeft, bottomRight, circlePos)
     alignment.horizontalSide match {
       case Side.Left =>     testEdge.x = topLeft.x; invVec.x = -1
       case Side.Right =>    testEdge.x = bottomRight.x; invVec.x = -1
