0354744a67a00c5aabb861a9793962be9bf5773e
luca giulianini <luca.giulianini2@studio.unibo.it>
2020-10-22 19:57:44 +0200

    Refactored mediator

--- src/main/scala/it/unibo/pps1920/motoscala/controller/mediation/Displayable.scala
+++ src/main/scala/it/unibo/pps1920/motoscala/controller/mediation/Displayable.scala
@@ -12,12 +12,6 @@ trait Displayable extends EventObserver[DisplayableEvent] {
    */
   def notifyDrawEntities(players: Set[Option[EntityData]], entities: Set[EntityData]): Unit
 
-  /** Notify the observer with data from [[it.unibo.pps1920.motoscala.controller.mediation.Event.LevelEndEvent]]
-   *
-   * @param data the data containing end level information
-   */
-  def notifyLevelEnd(data: EndData): Unit
-
   /** Notify the observer with data from [[it.unibo.pps1920.motoscala.controller.mediation.Event.EntityLifeEvent]]
    *
    * @param data the data containing life information
@@ -30,10 +24,16 @@ trait Displayable extends EventObserver[DisplayableEvent] {
    */
   def notifyRedirectSound(event: SoundEvent): Unit
 
+  /** Notify the observer with data from [[it.unibo.pps1920.motoscala.controller.mediation.Event.LevelEndEvent]]
+   *
+   * @param data the data containing end level information
+   */
+  def notifyLevelEnd(data: EndData): Unit
+
   override def notify(event: DisplayableEvent): Unit = event match {
     case DrawEntityEvent(player, entities) => notifyDrawEntities(player, entities)
-    case LevelEndEvent(data) => notifyLevelEnd(data)
     case EntityLifeEvent(data) => notifyEntityLife(data)
     case RedirectSoundEvent(event) => notifyRedirectSound(event)
+    case LevelEndEvent(data) => notifyLevelEnd(data)
   }
 }
